8fcd9e33785da9a9c67f8b8f1f5c0361e52e0b65
Yelam <kyl3476@naver.com>
2022-04-18 22:19:37 +0900

    깃 만들고 첫 번째 업로드

---
+++ src/views/AboutView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <h1>This is an about page. This is so hard.</h1>
+    <button @click="clickBtn">Test</button><br>
+    <button @click="buttonnum">Count</button><br>
+    <span>버튼을 클릭한 횟수 : {{buttonclick}}</span>
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+     return{
+        buttonclick:0
+     }
+    },
+    methods:{
+     clickBtn(){
+        alert('테스트');
+      },
+     buttonnum(){
+       //함수는 buttonnum function(){}나 위 방법이나 동일함
+        this.buttonclick+=1;
+     }
+  }
+}
+</script>
